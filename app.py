579140348581012873ee619153ad4bd4c7d1a6f8
Polina Kalimova <polina@example.com>
2025-10-28 06:39:20 +0400

    Add Bad Code

--- app.py
+++ app.py
@@ -167,6 +167,17 @@ def forbidden(e):
 
 debug = os.environ.get('FLASK_DEBUG', '0') == '1'
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    try:
+        from waitress import serve
+        if debug:
+            print("Запуск Flask (dev)")
+            app.run(debug=True)
+        else:
+            print("Запуск через waitress 0.0.0.0:5000")
+            serve(app, host='0.0.0.0', port=5000)
+    except Exception as e:
+        print("Waitress не доступен, запуск через Flask:", e)
+        app.run(debug=debug)
+
 
